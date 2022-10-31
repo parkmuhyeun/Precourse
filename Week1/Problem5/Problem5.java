16dde80eaee2fec7a375b7ce2a983c1cd883d5e0
parkmuhyeun <pjhg410@gmail.com>
2022-10-31 23:49:22 +0900

    [Week1] Problem5

---
+++ Week1/Problem5/Problem5.java
@@ -0,0 +1,12 @@
+package onboarding;
+
+import problem5.Bank;
+
+import java.util.List;
+
+public class Problem5 {
+    public static List<Integer> solution(int money) {
+        Bank bank = new Bank(money);
+        return bank.withdraw();
+    }
+}
