757d7f7bf2a0d5b33b50f19ff7bffad00aeea2ca
parkmuhyeun <pjhg410@gmail.com>
2022-11-30 21:42:56 +0900

    [Week2] refactor: 종료 메시지 출력 분리

--- Week2/src/BaseballController.java
+++ Week2/src/BaseballController.java
@@ -22,7 +22,7 @@ public class BaseballController {
             User user = new User();
 
             playGame(computer, user);
-            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            outputView.outputEndMessage();
             System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
             String select = inputSelect();
             validateSelect(select);
